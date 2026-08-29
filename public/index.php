0f66d45d0ac6d5dc52f1114bb33a28b344f44e02
Aleck Marcial <aleckmarcial@gmail.com>
2026-08-29 22:03:39 +0800

    about section added

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@
 <main>
     <section id="hero-section">
         <div class="hero-container">
-            <img src="img/hero-bg.png" alt="Rapide auto service shop front">
+            <img src="img/bg.png" alt="Rapide auto service shop front">
             <div class="hero-text">
                 <h1>Experience the Rapide Way!</h1>
                 <p>CASA-quality services at affordable prices.</p>
@@ -14,11 +14,23 @@
     </section>
 
     <section id="about">
-        <div>
-            <img src="" alt="">
+        <div class="about-section">
+            <div class="aboutImg-container">
+                <img src="img/rapide.jpg" alt="Rapide auto service about">
+            </div>
+            <div class="description">
+                <h1>Our History</h1>
+                <p>Rapidé is born of a joint venture with Midas International. Being the largest auto service center chain in the world, Midas provided the basis for Rapidé’s systems and procedures. This is among the things that helped us come up with the Rapidé Way. And it’s something that we’re proud of to this day. For 20 years, the Rapidé brand has stood the test of time. Here at Rapidé, your satisfaction is our guarantee.</p>
+            </div>
         </div>
-        <div>
-            
+        <div class="about-section">
+            <div class="miko-container">
+                <img src="img/miko2.png" alt="">
+            </div>
+            <div class="description">
+                <h1>Our Advantages</h1>
+                <p>Rapidé’s quality assurance can be summed up in these words: CASA-quality services at affordable prices. Our team of skilled technicians, coupled with state of the art equipment, allow us to fulfil this vision. This vision is what we now refer to as the Rapidé Way, and it’s something that separates us from every other competitor out there. Curious about the #RapidéWay and what makes it so good? Come and experience it for yourself!</p>
+            </div>
         </div>
     </section>
 
@@ -30,6 +42,5 @@
 
     </section>
 </main>
-
 
 <?php include 'includes/footer.php'; ?>
